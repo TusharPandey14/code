d5aeb02ec6bc3119a6737501d37922e4036bf054
TusharPandey14 <tusharpandey1410@gmail.com>
2023-07-19 21:23:07 +0530

    code

---
+++ demo.cpp
@@ -0,0 +1,21 @@
+using Ull = unsigned long long;
+using ll = long long;
+#include<bits/stdc++.h>
+using namespace std;
+ 
+ void solve(){ 
+ 
+
+}
+
+  int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL); 
+    int t; cin>>t; 
+    while(t--){
+        
+        solve();
+    }
+    
+    return 0;
+}
